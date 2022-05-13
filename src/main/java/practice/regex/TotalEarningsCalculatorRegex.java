b4254b41e3238a8e425e16a39303424c76379043
Pavel Martinkov <pavel.martinkov98@mail.ru>
2022-05-13 20:29:31 +0300

    task 3

--- src/main/java/practice/regex/TotalEarningsCalculatorRegex.java
+++ src/main/java/practice/regex/TotalEarningsCalculatorRegex.java
@@ -1,14 +1,27 @@
 package practice.regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TotalEarningsCalculatorRegex {
 
   public static void main(String[] args) {
-
+    String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+    System.out.println(calculateSalarySum(text));
   }
 
-  public static int calculateSalarySum(String text){
-    //TODO: реализуйте метод
-    return -1;
+  public static int calculateSalarySum(String text) {
+    int sum = 0;
+    String regex = "[0-9]+";
+    Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(text);
+    while (matcher.find()) {
+      int start = matcher.start();
+      int end = matcher.end();
+      String result = text.substring(start, end);
+      int count = Integer.parseInt(result);
+      sum = sum + count;
+    }
+    return sum;
   }
-
 }
