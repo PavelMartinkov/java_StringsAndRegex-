05c8b21ed154f3ce45656115283908a3a7028364
Pavel Martinkov <pavel.martinkov98@mail.ru>
2022-05-14 17:15:50 +0300

    task 5

--- src/main/java/practice/regex/FullNameFormatterRegex.java
+++ src/main/java/practice/regex/FullNameFormatterRegex.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class FullNameFormatterRegex {
 
   public static void main(String[] args) {
-
     Scanner scanner = new Scanner(System.in);
     while (true) {
       String input = scanner.nextLine();
@@ -13,9 +12,31 @@ public class FullNameFormatterRegex {
         scanner.close();
         break;
       }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+
+      int spaceCount = 0;
+      for (int i = 0; i < input.length(); i++) {
+        char symbol = input.charAt(i);
+        if (symbol == ' ') {
+          spaceCount++;
+        } else if (Character.isDigit(symbol)) {
+          break;
+        }
+      }
+
+      if (spaceCount == 2 || input.equals(Character.UnicodeBlock.CYRILLIC)) {
+        System.out.println(getFullName(input));
+      } else {
+        System.out.println("Введенная строка не является ФИО");
+        break;
+      }
     }
   }
 
+  public static String getFullName (String fullName) {
+    String[] words = fullName.split("\\s");
+    for (int i = 0; i < words.length; i++) {
+      return "Фамилия: " + words[0] + "\n" + "Имя: " + words[1] + "\n" + "Отчество: " + words[2] + "\n";
+    }
+    return fullName;
+  }
 }
