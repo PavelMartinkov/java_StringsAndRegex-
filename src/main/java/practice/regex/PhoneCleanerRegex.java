ead0064ffe6172386623133a3c4819895f5a203e
Pavel Martinkov <pavel.martinkov98@mail.ru>
2022-05-14 20:51:58 +0300

    task 6

--- src/main/java/practice/regex/PhoneCleanerRegex.java
+++ src/main/java/practice/regex/PhoneCleanerRegex.java
@@ -13,8 +13,43 @@ public class PhoneCleanerRegex {
         scanner.close();
         break;
       }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
+
+      String regex = "[^0-9]";
+      String phone = input.replaceAll(regex, "");
+      int countDigit = 0;
+      for (int i = 0; i < input.length(); i++) {
+        char symbol = input.charAt(i);
+        if (Character.isDigit(symbol)) {
+          countDigit++;
+        }
+        }
+
+        if (countDigit < 10 || countDigit > 11) {
+        System.out.println("Неверный формат номера");
+        break;
+        }
+
+        else if (phone.charAt(0) == '8') {
+          String phone2 = phone.replaceFirst("8", "7");
+          System.out.println(phone2);
+          break;
+        }
+
+        else if (countDigit == 10) {
+        String phone3 = phone.replaceFirst("", "7");
+        System.out.println(phone3);
+        break;
+        }
+
+        else if (phone.charAt(0) != '7') {
+          System.out.println("Неверный формат номера");
+          break;
+        }
+
+        else {
+        System.out.println(phone);
+        }
     }
   }
-
 }
+
