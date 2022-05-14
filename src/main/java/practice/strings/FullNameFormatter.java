4544ea897ad711855777bfbfc14d259ba624a695
Pavel Martinkov <pavel.martinkov98@mail.ru>
2022-05-14 16:54:36 +0300

    task 2

--- src/main/java/practice/strings/FullNameFormatter.java
+++ src/main/java/practice/strings/FullNameFormatter.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class FullNameFormatter {
 
   public static void main(String[] args) {
-
     Scanner scanner = new Scanner(System.in);
     while (true) {
       String input = scanner.nextLine();
@@ -13,10 +12,32 @@ public class FullNameFormatter {
         scanner.close();
         break;
       }
-      
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+
+      int spaceCount = 0;
+      for (int i = 0; i < input.length(); i++) {
+        char symbol = input.charAt(i);
+        if (symbol == ' ') {
+          spaceCount++;
+        } else if (Character.isDigit(symbol)) {
+          break;
+        }
+      }
+
+      if (spaceCount == 2 || input.equals(Character.UnicodeBlock.CYRILLIC)) {
+        System.out.println(getFullName(input));
+      } else {
+        System.out.println("Введенная строка не является ФИО");
+        break;
+      }
     }
   }
 
+  public static String getFullName (String fullName) {
+    int one = fullName.indexOf(' ');
+    int two = fullName.lastIndexOf(' ');
+    String familyName = fullName.substring(0, one);
+    String name = fullName.substring(one, two).trim();
+    String surname = fullName.substring(two).trim();
+    return "Фамилия: " + familyName + "\n" + "Имя: " + name + "\n" + "Отчество: " + surname + "\n";
+  }
 }
